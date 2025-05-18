4f6eb539a0ce6c4ebad0c8e8f1ddc35a91ad7fdf
Wieser Hannes - Studierendenaccount <hannes.wieser@student.uibk.ac.at>
2025-05-18 14:53:16 +0200

    Start of Notizen App

---
+++ notizen-app/src/test/java/com/example/notizen_app/NotizenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notizen_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotizenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
